0ca634ceedec05380f214bfef0fa3b6b48a41167
kirubeldev <kirubelmenberu35@gmail.com>
2025-04-05 12:30:30 +0300

    gg

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,22 +1,23 @@
 // api.js
 
-const API_KEY = "2a29d3d3c3a4259249962820059266fb";
-const BASE_URL = "http://api.themoviedb.org/3";
+const API_KEY = "2a29d3d3c3a4259249962820059266fb";// In your API service file (e.g., services/api.js)
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const response = await fetch(
+    `${BASE_URL}/movie/popular?api_key=2a29d3d3c3a4259249962820059266fb`
+  );
   const data = await response.json();
   return data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+    `${BASE_URL}/search/movie?api_key=2a29d3d3c3a4259249962820059266fb&query=${encodeURIComponent(query)}`
   );
   const data = await response.json();
   return data.results;
 };
-
 export const getMovieDetail = async (id) => {
   try {
     const response = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
